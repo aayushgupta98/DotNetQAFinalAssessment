76abf8d52ebe9327e7019bdbb6a4235249a58eee
aayush.gupta <aayush.gupta@3pillarglobal.com>
2021-01-04 13:44:20 +0530

    #1: added comments

--- TestInitialization.cs
+++ TestInitialization.cs
@@ -5,20 +5,40 @@ namespace DotNetQAFinalAssessment
 {
     public class TestInitialization
     {
+        /// <summary>
+        /// web driver
+        /// </summary>
         IWebDriver webDriver;
+
+        /// <summary>
+        /// initializing browser
+        /// </summary>
         public void Init_Browser()
         {
             webDriver = new ChromeDriver();
             webDriver.Manage().Window.Maximize();
         }
+
+        /// <summary>
+        /// navigating to specified url
+        /// </summary>
+        /// <param name="url"></param>
         public void Goto(string url)
         {
             webDriver.Url = url;
         }
+
+        /// <summary>
+        /// closing browser
+        /// </summary>
         public void Close()
         {
             webDriver.Quit();
         }
+
+        /// <summary>
+        /// returning instance of webdriver
+        /// </summary>
         public IWebDriver getDriver
         {
             get { return webDriver; }
